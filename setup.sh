52763157011607854a6157d83e2773b01df82289
Isaac Hailperin <isaac.hailperin@gmail.com>
2015-02-07 06:29:07 -0500

    added database creation to setup

--- setup.sh
+++ setup.sh
@@ -2,13 +2,53 @@
 # setup.sh
 
 # install dependencies
-sudo yum install numpy scipy python-matplotlib ffmpeg portaudio-devel gcc mariadb-server
-sudo pip install --allow-external PyAudio --allow-unverified PyAudio PyAudio
-sudo pip install pydub 
-sudo pip install PyDejavu
-sudo mysql_secure_installation
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
+install_dependencies(){
+    sudo yum install numpy scipy python-matplotlib ffmpeg portaudio-devel gcc mariadb-server
+    sudo pip install --allow-external PyAudio --allow-unverified PyAudio PyAudio
+    sudo pip install pydub 
+    sudo pip install PyDejavu
+    sudo mysql_secure_installation
+    sudo systemctl enable mariadb
+    sudo systemctl start mariadb
+}
+
+# create settings.py file with the db credentials
+create_settings(){
+    user_name=$1
+    user_pw=$2
+    db_name=$3
+    cat >>settings.py<<EOF
+# settings.py
+# settins file for mud
+
+# dejavu db settings
+dejavu_config = {
+     'database': {
+         'host': '127.0.0.1',
+         'user': '${user_name}',
+         'passwd': '${user_pw}', 
+         'db': ${db_name},
+     }
+ }
+EOF
+    
+}
+
 # setup database
+# assuming local database
+setup_database(){
+    echo "Warning: if someone else is logged in to this system right now, it might be"
+    echo "able to read the MariaDB root password from the process list"
+    echo
+    read -ers -p "Please enter MariaDB root password:" rootpw
+    db_name="dejavu"
+    mysql -u root --password=${rootpw} -e "CREATE DATABASE IF NOT EXISTS ${db_name};"
+    user_name="dejavu-admin"
+    user_pw=$(openssl rand -base64 23)
+    mysql -u root --password=${rootpw} -e "grant all on dejavu.* to '${user_name}'@'localhost' identified by '${user_pw}';"
+    create_settings ${user_name} ${user_pw} ${db_name}
+    
+}
 
-
+#install_dependencies
+setup_database
