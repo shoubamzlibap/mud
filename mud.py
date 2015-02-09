dcc1c1ca3c0847580ee155d6ecd65f70e8f7a6db
Isaac Hailperin <isaac.hailperin@gmail.com>
2015-02-09 05:00:20 -0500

    first basic working version

--- mud.py
+++ mud.py
@@ -9,17 +9,158 @@ VERSION=0.1 # MAR-2015 | isaac | initial version
 
 import argparse
 import os
+from MySQLdb import IntegrityError
+import warnings
 import settings
 
-#from dejavu import Dejavu
-#from dejavu.recognize import FileRecognizer
-from dejavu.database_sql import SQLDatabase
+from dejavu import Dejavu
+#from dejavu.database_sql import Database, SQLDatabase, 
+from dejavu.database_sql import SQLDatabase, cursor_factory, DictCursor
+from dejavu.recognize import FileRecognizer
 
 class MudDatabase(SQLDatabase):
     """
     Shamelessly stealing database code from dejavu.
     """
-    pass
+    # tables
+    SONGFILES_TABLENAME = "songfiles"
+
+    # fields
+    FIELD_FILE_ID = 'file_id' # primary key, autoincrement
+    FIELD_FILE_PATH = 'file_path'
+    FIELD_SONG_ID = SQLDatabase.FIELD_SONG_ID  # foreign key -> songs, song_id
+    FIELD_SONG_ARTIST = 'song_artist'
+    FIELD_SONG_NAME = 'song_name'
+    FIELD_SONG_ALBUM = 'song_album'
+
+    # creates
+    CREATE_SONGFILES_TABLE = """
+        CREATE TABLE IF NOT EXISTS `%s` (
+             `%s` mediumint unsigned not null auto_increment,
+             `%s` varchar(500) not null unique,
+             `%s` mediumint unsigned,
+             `%s` varchar(250),
+             `%s` varchar(250),
+             `%s` varchar(250),
+         INDEX (%s,%s),
+         UNIQUE KEY `unique_constraint` (%s, %s),
+         FOREIGN KEY (%s) REFERENCES %s(%s) ON DELETE CASCADE
+    ) ENGINE=INNODB;""" % (
+        SONGFILES_TABLENAME, 
+        FIELD_FILE_ID,
+        FIELD_FILE_PATH, 
+        FIELD_SONG_ID,
+        FIELD_SONG_ARTIST,
+        FIELD_SONG_NAME,
+        FIELD_SONG_ALBUM,
+        FIELD_FILE_PATH, FIELD_SONG_ID, # index
+        FIELD_FILE_ID, FIELD_FILE_PATH,  # uniq keys
+        FIELD_SONG_ID, SQLDatabase.SONGS_TABLENAME, FIELD_SONG_ID# forein key
+
+    )
+
+    # inserts
+    INSERT_SONGFILE = """ 
+        INSERT INTO %s (%s) values
+        (%%s); """ % (
+        SONGFILES_TABLENAME, FIELD_FILE_PATH
+    )
+
+    # updates
+    UPDATE_SONGFILE = """
+        UPDATE %s SET %s=%%s
+        WHERE %s=%%s;""" % (SONGFILES_TABLENAME, FIELD_SONG_ID, 
+        FIELD_FILE_PATH
+    )
+
+    # selects
+    SELECT_NEW_FILES = """
+        SELECT %s FROM %s WHERE %s is NULL;
+        """ %(
+        FIELD_FILE_PATH, SONGFILES_TABLENAME, FIELD_SONG_ID
+    )
+
+    SELECT_SONG_IDS = """ SELECT %s FROM %s 
+        ;""" %(FIELD_SONG_ID, SQLDatabase.SONGS_TABLENAME)
+
+    SELECT_FILE_BY_ID = """ 
+        SELECT %s FROM %s WHERE %s=%%s
+        """ %(FIELD_FILE_PATH, SONGFILES_TABLENAME, FIELD_SONG_ID)
+
+    def __init__(self, **options):
+        """
+        Setup Database code
+        """
+        self.cursor = cursor_factory(**options)
+
+
+    def setup(self):
+        """
+        Creates any non-existing tables required for mud to function.
+
+        This also removes all songs that have been added but have no
+        fingerprints associated with them.
+        """
+        # first calling dejavu setup, as we depend on these databases
+        super(MudDatabase, self).setup()
+        with self.cursor() as cur:
+            cur.execute(self.CREATE_SONGFILES_TABLE)
+
+    def insert_songfile(self, file_path):
+        """
+        Insert a song file into the database
+        """
+        with self.cursor() as cur:
+            cur.execute(self.INSERT_SONGFILE, (file_path))
+
+    def update_songfile(self, file_path, song_id):
+        """
+        Update a songfile with its song id
+        """
+        with self.cursor() as cur:
+            cur.execute(self.UPDATE_SONGFILE, (song_id, file_path))
+
+    def select_new_files(self):
+        """
+        Select all files without a song ID
+        """
+        with self.cursor(cursor_type=DictCursor) as cur:
+            cur.execute(self.SELECT_NEW_FILES)
+            for row in cur:
+                yield row
+
+    def select_song_ids(self):
+        """
+        Get all song IDs
+        """
+        with self.cursor(cursor_type=DictCursor) as cur:
+            cur.execute(self.SELECT_SONG_IDS)
+            for row in cur:
+                yield row
+
+    def select_file_by_id(self, song_id):
+        """
+        Get songfile by id
+        """
+        with self.cursor(cursor_type=DictCursor) as cur:
+            cur.execute(self.SELECT_FILE_BY_ID, (song_id))
+            for row in cur:
+                yield row
+
+#    def execute_query(self,query):
+#        """
+#        Execute an arbitrary query
+#        """
+#        with self.cursor(cursor_type=DictCursor) as cur:
+#            cur.execute(query)
+#            for row in cur:
+#                yield row
+
+
+# object for usage by functions below
+warnings.filterwarnings('ignore')
+db = MudDatabase(**settings.dejavu_config.get('database',{}))
+djv = Dejavu(settings.dejavu_config)
 
 def build_collection():
     """
@@ -30,8 +171,12 @@ def build_collection():
     to the song_id in dejavu.songs
 
     """
+    iprint('Building collection')
     for song_f in list_new_files():
+        print
+        print song_f
         song_id = get_song_id(song_f)
+        print song_id
         add_to_collection(song_f, song_id)
 
 def add_to_collection(song_file, song_id):
@@ -42,14 +187,15 @@ def add_to_collection(song_file, song_id):
     song_id: int, foreign key to songs database
 
     """
-    pass
-
+#TODO: add mp3 tags to database, integrate mp3 tags in print function
+    db.update_songfile(song_file, song_id)
 
 def list_new_files():
     """
-    Return a list of filenames that are not yet fingerprinted.
+    Return a list of filenames from the database that are not yet fingerprinted.
     """
-    pass
+    for f in db.select_new_files():
+        yield f['file_path']
 
 def get_song_id(song_file):
     """
@@ -57,8 +203,10 @@ def get_song_id(song_file):
 
     song_file: string, absolute path to sound file
     """
-    pass
-
+    djv.fingerprint_file(song_file)
+    song = djv.recognize(FileRecognizer, song_file)
+    return song['song_id']
+    
 def scan_files():
     """
     Scan for music files and add them to the database
@@ -74,7 +222,10 @@ def add_song_file(song_file):
     """
     Add a song file to the database, if it not already exists.
     """
-    pass
+    try:
+        db.insert_songfile(song_file)
+    except IntegrityError:
+        pass
 
 def get_duplicates():
     """
@@ -85,7 +236,26 @@ def get_duplicates():
     return song_ids (together with the song_files) that have more
     then one song_file pointing to them.
     """
-    pass
+# TODO: fail if no files with song_ids in database
+    duplicates = []
+    for sid in db.select_song_ids():
+        files = []
+        for path in db.select_file_by_id(sid['song_id']):
+            files.append(path)
+        if len(files) > 1:
+            duplicates.append(files)
+    return duplicates
+
+def print_duplicates():
+    """
+    Print duplicates to std out
+    """
+    dups = get_duplicates()
+    for sfiles in dups:
+        print
+        for f in sfiles:
+            print f['file_path']
+    
 
 def iprint(message, level=2):
     """
@@ -103,14 +273,28 @@ def iprint(message, level=2):
 ###
 if __name__ == '__main__':
     
+# TODO: add option to verify files in data - do they still exist? If not, delete
     parser = argparse.ArgumentParser(
         description = 'Check for duplicates in your music collection. mud will find \
             all duplicates, even if the file is encoded at a different bitrate. And \
             of course it won\'t be fooled by tags :)')
-    parser.add_argument('-b','--build-collection',
-        action = 'store_true',
-        help = 'Go through collection and build database')
     parser.add_argument('-s','--scan',
         action = 'store_true',
         help = 'Scan music directory for new files')
+    parser.add_argument('-b','--build-collection',
+        action = 'store_true',
+        help = 'Go through collection and build database')
+    parser.add_argument('-p','--print-dupes',
+        action = 'store_true',
+        help = 'print all duplicates found')
     args = parser.parse_args()
+
+    db.setup()
+    if args.scan:
+        scan_files()
+    if args.build_collection:
+        build_collection()    
+    if args.print_dupes:
+        print_duplicates()
+
+
--- tests/test_mud.py
+++ tests/test_mud.py
@@ -4,6 +4,8 @@ import sys
 import subprocess
 import time
 import mock
+import warnings
+from MySQLdb import IntegrityError
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if not path in sys.path:
@@ -11,14 +13,11 @@ if not path in sys.path:
 del path
 
 import settings
-import mud
 
 class testMud(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        # general purpose mock object
-        #self.gp_mock = mock.Mock()
         # set base dir, possibly overwriting value in settings
         self.music_base_dir = '/tmp/mud'
         settings.music_base_dir = self.music_base_dir
@@ -31,10 +30,36 @@ class testMud(unittest.TestCase):
             os.makedirs(self.music_base_dir + d)
         for f in self.files:
             open(self.music_base_dir + f, 'w').close()
+        # test song
+        self.test_song = '/home/isaac/lucky_chops_renc/Lucky Chops/Lucky Chops/Lucky Chops - Lucky Chops - 01 Hey Soul Sister.mp3'
+        # database creation
+        self.test_db = 'mudtest'
+        settings.dejavu_config['database']['db'] = self.test_db
+        test_db_user = settings.dejavu_config['database']['user']
+        test_db_pw = settings.dejavu_config['database']['passwd']
+        # NOTE: set the path to your db root pw file here - (do not test in production ;))
+        db_root_pw_file = open('/home/isaac/.db_root_pw', 'r')
+        self.db_root_pw = db_root_pw_file.read()
+        db_root_pw_file.close()
+        create_db_command = 'mysql -u root --password=' + self.db_root_pw + ' -e'
+        create_db_command = create_db_command.split() + ['CREATE DATABASE IF NOT EXISTS ' + self.test_db + ';']
+        grant_all_command = 'mysql -u root --password=' + self.db_root_pw + ' -e'
+        grant_all_command = grant_all_command.split() + \
+        ['grant all on ' + self.test_db + '.* to \'' + test_db_user + '\'@\'localhost\' identified by \'' + test_db_pw + '\';']
+        subprocess.call(create_db_command)
+        subprocess.call(grant_all_command)
+        # create database object for later usage
+        warnings.filterwarnings('ignore')
+        import mud
+        self.db = mud.MudDatabase(**settings.dejavu_config.get('database',{}))
+        self.db.setup()
 
     #@classmethod
     def tearDown(self):
         subprocess.call(['rm', '-rf', self.music_base_dir])
+        drop_db_command = 'mysql -u root --password=' + self.db_root_pw + ' -e'
+        drop_db_command = drop_db_command.split() + ['DROP DATABASE ' + self.test_db + ';']
+        subprocess.call(drop_db_command)
 
     gp_mock = mock.Mock()
     @mock.patch('mud.add_song_file', gp_mock.add_song_file )
@@ -42,10 +67,64 @@ class testMud(unittest.TestCase):
         """
         Files in testdir scanned correctly
         """
+        import mud
         mud.scan_files()
         for f in [g for g in self.files if g.endswith('.mp3') ]:
             self.gp_mock.add_song_file.assert_any_call(self.music_base_dir + f)
 
-    @unittest.skip('TODO: implement this')
-    def test_add_song_file(self):
-        pass
+    def test_insertfiles(self):
+        """
+        Files are inserted and retrieved correctly from db
+        """
+        import mud
+        # insert file - expecting no Exceptions
+        for f in self.files:
+            mud.add_song_file(f)
+        # insert file twice (should not add file twice, checked below)
+        for f in self.files:
+            mud.add_song_file(f)
+        # select new files
+        new_files = []
+        for f in mud.list_new_files():
+            new_files.append(f)
+        self.assertListEqual(sorted(new_files), sorted(self.files))
+
+
+    @unittest.skip('Tested successfully, runs very long')
+    def test_get_song_id(self):
+        """
+        Song ID returned correctly
+        """
+        import mud
+        # add song, songid shoudl be 1
+        sid = mud.get_song_id(self.test_song)
+        self.assertEqual(sid, 1)
+        # add song again, songid shoudl be 1
+        sid = mud.get_song_id(self.test_song)
+        self.assertEqual(sid, 1)
+
+
+    def test_update_songfile(self):
+        """
+        Song ID updated correctly
+        """
+        import mud
+        test_file = self.files[0]
+        song_id = 23
+        mud.add_song_file(test_file)
+        # just asserting no Exception raised
+        with self.assertRaises(IntegrityError):
+            mud.add_to_collection(test_file, song_id )
+
+#    @unittest.skip('runs very long')
+    def test_get_duplicates(self):
+        """
+        Duplicates are returned correctly
+        """
+        import mud
+        settings.music_base_dir = '/home/isaac'
+        mud.scan_files()
+        mud.build_collection()
+        dups = mud.get_duplicates()
+        print dups
+
