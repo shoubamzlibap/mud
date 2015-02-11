7d0c84b8e6c2d95812ddc942eb33e87991fd7e80
Isaac Hailperin <isaac.hailperin@gmail.com>
2015-02-11 12:44:58 -0500

    fixed some testing issues

--- tests/test_mud.py
+++ tests/test_mud.py
@@ -31,16 +31,15 @@ class testMud(unittest.TestCase):
         for f in self.files:
             open(self.music_base_dir + f, 'w').close()
         # test song
-        self.test_song = '/home/isaac/lucky_chops_renc/Lucky Chops/Lucky Chops/Lucky Chops - Lucky Chops - 01 Hey Soul Sister.mp3'
+        self.test_song = '/home/isaac/lucky_chops_renc/Lucky Chops/Lucky Chops - Lucky Chops - 08 Lean On Me.mp3'
         # database creation
         self.test_db = 'mudtest'
         settings.dejavu_config['database']['db'] = self.test_db
         test_db_user = settings.dejavu_config['database']['user']
         test_db_pw = settings.dejavu_config['database']['passwd']
         # NOTE: set the path to your db root pw file here - (do not test in production ;))
-        db_root_pw_file = open('/home/isaac/.db_root_pw', 'r')
-        self.db_root_pw = db_root_pw_file.read()
-        db_root_pw_file.close()
+        with open('/home/isaac/.db_root_pw', 'r') as db_root_pw_file:
+            self.db_root_pw = db_root_pw_file.read()
         create_db_command = 'mysql -u root --password=' + self.db_root_pw + ' -e'
         create_db_command = create_db_command.split() + ['CREATE DATABASE IF NOT EXISTS ' + self.test_db + ';']
         grant_all_command = 'mysql -u root --password=' + self.db_root_pw + ' -e'
@@ -48,6 +47,9 @@ class testMud(unittest.TestCase):
         ['grant all on ' + self.test_db + '.* to \'' + test_db_user + '\'@\'localhost\' identified by \'' + test_db_pw + '\';']
         subprocess.call(create_db_command)
         subprocess.call(grant_all_command)
+        # get mud
+        import mud
+        self.mud = mud
         # create database object for later usage
         warnings.filterwarnings('ignore')
         import mud
@@ -67,8 +69,7 @@ class testMud(unittest.TestCase):
         """
         Files in testdir scanned correctly
         """
-        import mud
-        mud.scan_files()
+        self.mud.scan_files()
         for f in [g for g in self.files if g.endswith('.mp3') ]:
             self.gp_mock.add_song_file.assert_any_call(self.music_base_dir + f)
 
@@ -76,16 +77,15 @@ class testMud(unittest.TestCase):
         """
         Files are inserted and retrieved correctly from db
         """
-        import mud
         # insert file - expecting no Exceptions
         for f in self.files:
-            mud.add_song_file(f)
+            self.mud.add_song_file(f)
         # insert file twice (should not add file twice, checked below)
         for f in self.files:
-            mud.add_song_file(f)
+            self.mud.add_song_file(f)
         # select new files
         new_files = []
-        for f in mud.list_new_files():
+        for f in self.mud.list_new_files():
             new_files.append(f)
         self.assertListEqual(sorted(new_files), sorted(self.files))
 
@@ -95,36 +95,34 @@ class testMud(unittest.TestCase):
         """
         Song ID returned correctly
         """
-        import mud
         # add song, songid shoudl be 1
-        sid = mud.get_song_id(self.test_song)
+        sid = self.mud.get_song_id(self.test_song)
         self.assertEqual(sid, 1)
         # add song again, songid shoudl be 1
-        sid = mud.get_song_id(self.test_song)
+        sid = self.mud.get_song_id(self.test_song)
         self.assertEqual(sid, 1)
 
-
+    my_mock = mock.Mock()
+    @mock.patch('eyed3.load', my_mock.fake_load)
     def test_update_songfile(self):
         """
         Song ID updated correctly
         """
-        import mud
         test_file = self.files[0]
         song_id = 23
-        mud.add_song_file(test_file)
+        self.mud.add_song_file(test_file)
         # just asserting no Exception raised
         with self.assertRaises(IntegrityError):
-            mud.add_to_collection(test_file, song_id )
+            self.mud.add_to_collection(test_file, song_id )
 
-#    @unittest.skip('runs very long')
+    @unittest.skip('Tested successfully, runs very long')
     def test_get_duplicates(self):
         """
         Duplicates are returned correctly
         """
-        import mud
         settings.music_base_dir = '/home/isaac'
-        mud.scan_files()
-        mud.build_collection()
-        dups = mud.get_duplicates()
+        self.mud.scan_files()
+        self.mud.build_collection()
+        dups = self.mud.get_duplicates()
         print dups
 
